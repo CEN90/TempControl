7c0415356808ac248dc65ab3052cb54a619311ab
CEN90 <christopher.nordin@outlook.com>
2023-08-30 10:18:16 +0200

    Fix output on build.ps1

--- build.ps1
+++ build.ps1
@@ -20,17 +20,19 @@ $out = '.\out\' + $name + '\'
 $out_cpp = $out + '*.cpp'
 $out_exe = $out + 'executable'
 $exe = $out_exe + '.exe'
+$out_xml = $out + 'output.xml'
 
 # process
 $executable = @{
     FilePath = "$exe"
     NoNewWindow = $true
     Wait = $true
+    RedirectStandardOutput = $out_xml
 }
 
 # Finished building the commands, now run them
 Write-Host "Running RMC on rebeca"
-java -jar $jar -s $Rebec -p $Prop -o $out -v $version -e $type
+java -jar $jar -s $Rebec -p $Prop -o $out -v $version -e $type -x
 Write-Output " "
 
 Write-Host "Compiling cpp"
@@ -38,3 +40,10 @@ g++ $out_cpp -w -o $out_exe
 
 Write-Host "Starting model check"
 Start-Process @executable
+
+$sys_info = '/model-checking-report/system-info'
+$check_prop = '/model-checking-report/checked-property'
+
+# If error then don't always need a full stack trace, show only summary
+Select-Xml -Path $out_xml -XPath $sys_info | ForEach-Object { $_.Node.InnerXML }
+Select-Xml -Path $out_xml -XPath $check_prop | ForEach-Object { $_.Node.InnerXML }
