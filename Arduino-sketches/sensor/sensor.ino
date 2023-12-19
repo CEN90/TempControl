2eec8eeed566e6edf38b572990d880e27fb2548a
CEN90 <christopher.nordin@outlook.com>
2023-12-19 14:38:05 +0100

    sensor

--- Arduino-sketches/sensor/sensor.ino
+++ Arduino-sketches/sensor/sensor.ino
@@ -16,28 +16,28 @@ TMP36 temp_main(TEMP_MAIN, 5.0); // 5.0 volt sensor
 TMP36 temp_aux(TEMP_AUX, 5.0); 
 
 void setup() {
-  Wire.begin(2);
-  Wire.onRequest(onRequestTemp);
+    Wire.begin(2);
+    Wire.onRequest(onRequestTemp);
 }
 
 void loop() {
-  readTemp();
-  delay(10);
+    readTemp();
+    delay(10);
 }
 
 void readTemp(){
-  float read_main = 0.0;
-  float read_aux = 0.0;
+    float read_main = 0.0;
+    float read_aux = 0.0;
 
-  for (int i = 0; i < RESOLUTION; i++) {
-    read_main += temp_main.getTempC();
-    read_aux += temp_aux.getTempC();
-  }
+    for (int i = 0; i < RESOLUTION; i++) {
+        read_main += temp_main.getTempC();
+        read_aux += temp_aux.getTempC();
+    }
 
-  temp.main = (int) round(read_main / 10.0);
-  temp.aux = (int) round(read_aux / 10.0);
+    temp.main = (int) round(read_main / 10.0);
+    temp.aux = (int) round(read_aux / 10.0);
 }
 
 void onRequestTemp() {
-  Wire.write((byte*)&temp, 2);
+    Wire.write((byte*)&temp, 2);
 }
