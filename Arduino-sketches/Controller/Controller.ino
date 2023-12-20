f60a0dbfad86f203a6f008f80f96b60391fa21b5
CEN90 <christopher.nordin@outlook.com>
2023-12-20 11:10:59 +0100

    Hacked controller

--- Arduino-sketches/Controller/Controller.ino
+++ Arduino-sketches/Controller/Controller.ino
@@ -1,57 +1,33 @@
-#include <TMP36.h>
+#include "controller.hpp"
 
-#define ATTACK           0
-
-#define ADC_RESOLUTION   1024.0
-#define POLL_TIME        1000
-
-// Pin definitions
-#define TEMP_IN          PIN_A0
-#define HEATING_PORT     8
-#define COOLING_PORT     9
-#define SWITCH_OFF_PORT  10
-
-#define SMOOTHING        10
-
-#define DESIRED_TEMP     22
-float current_temp;
-
-void setOutput(int attack);
-float readSensor(int sensor);
-float smoothInput(int sensor);
-float getTemperature(int sensor, int attack);
-void switchOffUnit();
-void setHeating();
-void setCooling();
-
+boolean hacked_prev = false;
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(BAUD);
 
-    pinMode(TEMP_IN, INPUT);
-    pinMode(HEATING_PORT, OUTPUT);
-    pinMode(COOLING_PORT, OUTPUT);
-    pinMode(SWITCH_OFF_PORT, OUTPUT);
+    Wire.begin();
 
-    // Attack part
-    if (ATTACK) {
-        randomSeed(analogRead(PIN_A2));
-    }
+    setPins();
 }
 
 void loop() {
+    if (digitalRead(ATTACK_BUTTON) && !hacked_prev) {
+        hacked_prev = true;
+        hacked();
+    }
+
     // Start with current temp
-    current_temp = getTemperature(TEMP_IN, ATTACK);
+    current_temp = getTemperature();
     Serial.print("Temp: ");
     Serial.println(current_temp);
-
-    setOutput(ATTACK); // Defer to separate function
+    
+    setOutput(); // Set output based on temp
 
     delay(POLL_TIME);
 }
 
 // If attack is set then scramble output
-void setOutput(int attack) {
+void setOutput() {
     if (current_temp == DESIRED_TEMP)
         switchOffUnit();
 
@@ -62,50 +38,20 @@ void setOutput(int attack) {
         setCooling();
 }
 
-float readSensor(int sensor) {
-    // convert the ADC value to voltage in millivolt
-    float voltage = 5.0 * (analogRead(sensor) / ADC_RESOLUTION);
+int getTemperature() {
+    int main = 0;
+    int aux = 0;
 
-    // convert the voltage to the temperature in Celsius
-    return (voltage - 0.5) * 100.0;
-}
+    Wire.requestFrom(SENSOR, SENSOR_DATA_SIZE);
 
-// Reading is erratic, smooth it out over multiple reads
-float smoothInput(int sensor) {
-    float total = 0;
+    while (Wire.available()) { // peripheral may send less than requested
+        main = Wire.read(); 
+        aux = Wire.read(); 
+    }    
 
-    // Add all reads then take the average from them
-    for (size_t i = 0; i < SMOOTHING; i++) {
-        total += readSensor(sensor);
-        delay(1);
+    if (aux >= SHUTOFF_TEMP) {
+        emergency_shutoff();
     }
 
-    return total / SMOOTHING;
-}
-
-// Return random temp if attacked, otherwise the temp
-float getTemperature(int sensor, int attack) {
-    if (attack) {
-        return random(50);
-    }
-    
-    return smoothInput(sensor);
-}
-
-void switchOffUnit() {
-    digitalWrite(SWITCH_OFF_PORT, HIGH);
-    digitalWrite(HEATING_PORT, LOW);
-    digitalWrite(COOLING_PORT, LOW);
-}
-
-void setHeating() {
-    digitalWrite(SWITCH_OFF_PORT, LOW);
-    digitalWrite(HEATING_PORT, HIGH);
-    digitalWrite(COOLING_PORT, LOW);
-}
-
-void setCooling() {
-    digitalWrite(SWITCH_OFF_PORT, LOW);
-    digitalWrite(HEATING_PORT, LOW);
-    digitalWrite(COOLING_PORT, HIGH);
+    return main;
 }
---
+++ Arduino-sketches/Controller/controller.hpp
@@ -0,0 +1,129 @@
+#include <Wire.h>
+
+#define POLL_TIME        1000
+#define BAUD             115200
+
+// Heating
+#define HEATING_LED      PIN7
+#define HEATING_ELEMENT  PIN6
+// Cooling
+#define COOLING_LED      PIN5
+#define COOLING_FAN      PIN4
+// Signal shut off
+#define SWITCH_OFF_LED   PIN3
+// Random seed source
+#define SEED             PIN_A4
+// Sensor adress
+#define SENSOR           2
+#define SENSOR_DATA_SIZE 2
+
+#define ATTACKS_LEN      4
+#define ATTACK_BUTTON    13
+
+// Temp definitions
+#define SHUTOFF_TEMP     35
+#define DESIRED_TEMP     25
+int current_temp;
+
+enum attacks_t {
+    block_heating,
+    block_cooling,
+    block_shutdown,
+    both_heat_cool,
+    none
+};
+
+
+// Description for attacks
+String attack_labels[ATTACKS_LEN] = {
+    "Blocking signal to heating element.",
+    "Blocking signal to cooling fan.",
+    "Blocking shutdown mechanism.",
+    "Turning on both heating element and cooling fan.",
+};
+
+attacks_t current_attack = none;
+
+boolean is_current_attack(attacks_t attack) {
+    return current_attack == attack;
+}
+
+void hacked() {
+    Serial.println(F("Oh, no. I've been hacked!"));
+    
+    randomSeed(analogRead(SEED)); // Seed
+
+    current_attack = (attacks_t) random(0, ATTACKS_LEN);
+
+    // Print out description of current attack
+    Serial.println(attack_labels[current_attack]); 
+}
+
+// Precaution if the box gets too hot
+void emergencyswitchoff() {
+    Serial.println(F("Too hot, shutting down!"));
+    switchOffUnit();
+
+    while (true) {
+        digitalWrite(HEATING_LED, HIGH);
+        delay(1000);
+        digitalWrite(HEATING_LED, LOW);
+        delay(1000);
+    }
+}
+
+void setPins() {
+    pinMode(HEATING_LED, OUTPUT);
+    pinMode(HEATING_ELEMENT, OUTPUT);
+
+    pinMode(COOLING_LED, OUTPUT);
+    pinMode(COOLING_FAN, OUTPUT);
+
+    pinMode(SWITCH_OFF_LED, OUTPUT);
+
+    pinMode(ATTACK_BUTTON, INPUT_PULLUP);
+}
+
+void switchOffUnit() {
+    digitalWrite(SWITCH_OFF_LED, HIGH);
+    digitalWrite(HEATING_LED, LOW);
+    digitalWrite(COOLING_LED, LOW);
+
+    // Set both low if not attack
+    if (!is_current_attack(block_shutdown)) {
+        digitalWrite(HEATING_ELEMENT, LOW);
+        digitalWrite(COOLING_FAN, LOW);
+    }
+
+    // Set both high if attack
+    if (is_current_attack(both_heat_cool)) {
+        digitalWrite(HEATING_ELEMENT, HIGH);
+        digitalWrite(COOLING_FAN, HIGH);
+    }
+}
+
+void setHeating() {
+    digitalWrite(SWITCH_OFF_LED, LOW);
+    digitalWrite(HEATING_LED, HIGH);
+    digitalWrite(COOLING_LED, LOW);
+
+    // Set heating high if not attack
+    if (!is_current_attack(block_heating)) {
+        digitalWrite(HEATING_ELEMENT, HIGH);
+    }
+
+    digitalWrite(COOLING_FAN, LOW);    
+}
+
+void setCooling() {
+    digitalWrite(SWITCH_OFF_LED, LOW);
+    digitalWrite(HEATING_LED, LOW);
+    digitalWrite(COOLING_LED, HIGH);
+
+    // Set fan high if not attack
+    if (!is_current_attack(block_cooling)) {
+        digitalWrite(COOLING_FAN, HIGH);
+    }
+
+    digitalWrite(HEATING_ELEMENT, LOW);
+}
